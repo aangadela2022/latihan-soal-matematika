03a30516b47c3ecb8692729238315d669735f54e
aangadela2022 <aangadela2022@users.noreply.github.com>
2026-04-06 00:54:44 +0700

    Fix vite config base path to resolve blank screen on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Menambahkan base relative path agar file JS dan CSS tidak 404/Kosong saat dihosting 
 })
