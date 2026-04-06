8390e21ffc36e439afcddea8fe4a162c33f571f5
aangadela2022 <aangadela2022@users.noreply.github.com>
2026-04-07 06:18:39 +0700

    Update base URL for standard GitHub Pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Menambahkan base relative path agar file JS dan CSS tidak 404/Kosong saat dihosting 
+  base: '/latihan-soal-matematika/', // Menggunakan nama repository yang tepat untuk GitHub Pages
 })
